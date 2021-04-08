c16d05cb75909d43a9e797d55548e696f13f15ac
Jose del Río <josedelrio85@github.com>
2021-04-08 12:43:28 +0200

    removed --profile flag

--- attach_instances_target_group/env_staging/exec.sh
+++ attach_instances_target_group/env_staging/exec.sh
@@ -8,8 +8,8 @@ for i in "${array[@]}"
 do
   di_cmd="aws ec2 describe-instances --filters \"Name=tag:Name,Values=$i\" \
   --query \"Reservations[*].Instances[*].{instance_id: InstanceId}\" \
-  --output json --region eu-west-2 --profile udrafter \
-  | jq '.[0][0] | {insid: .instance_id}'"
+  --output json --region eu-west-2 \
+  | jq '.[0][0] | {insid: .instance_id}'"  # add --profile udrafter if needed
 
   inst=$(eval "$di_cmd" | jq -r '.[]')
   instances+="Id=${inst} "
@@ -19,7 +19,7 @@ echo "Instances to attach: $instances"
 
 rt_cmd="aws elbv2 register-targets \
 --target-group-arn arn:aws:elasticloadbalancing:eu-west-2:333205879969:targetgroup/target-group-test/2d6e7b704bed5d46 \
---targets $instances --region eu-west-2 --profile udrafter"
+--targets $instances --region eu-west-2" # add --profile udrafter if needed
 
 z=$(eval "$rt_cmd")
 echo $z
